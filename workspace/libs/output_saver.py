d3640c7a0bcf098e582b3f8f3068acf53f207ca8
sh-song <shs@inha.edu>
2022-10-31 01:32:33 +0900

    [Saver] add image saving

--- workspace/libs/output_saver.py
+++ workspace/libs/output_saver.py
@@ -1,4 +1,10 @@
-
+import cv2
 class OutputSaver:
-    def __init__(self):
-        pass
+    def __init__(self, cfg):
+        self.cfg = cfg
+
+    def save_images(self, images, dirname):
+        path = f"{self.cfg.save_path}/{dirname}"
+        for i, img in enumerate(images):
+            cv2.imwrite(f"./{path}/{i}.png", img)
+        print(f"[Saver] Saved images in {path}")
