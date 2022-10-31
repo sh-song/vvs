8b6b1f0cf073aa0491bce039e5dc478565f44cf9
sh-song <shsong930@gmail.com>
2022-10-31 14:46:07 +0900

    [Loader] Load image data in asceding order

--- workspace/libs/data_loader.py
+++ workspace/libs/data_loader.py
@@ -4,12 +4,32 @@ class DataLoader:
     def __init__(self, cfg):
         self.cfg = cfg
 
+    def sort_filenames(self, filenames):
+        #Init sorted lsit
+        sorted = [(None, None)] * len(filenames) 
+
+        #Extract frame number from filename
+        for i, filename in enumerate(filenames):
+            frame = filename.split('.')[0]
+            sorted[i] = (int(frame), filename)
+
+        #Sort in ascending order
+        sorted.sort()
+
+        # Return filenames in sorted order
+        for i in range(len(filenames)):
+            filenames[i] = sorted[i][1]
+
+        return filenames
+
     def load_images(self, path):
         filenames = os.listdir(path)
+        filenames = self.sort_filenames(filenames)
         imgs = [None] * len(filenames)
          
         for i, filename in enumerate(filenames):
             imgs[i] = cv2.imread(f"{path}/{filename}")
+            print(f"{path}/{filename}")
         return imgs
 
     def left_images(self):
